6537b2c23afc6e91cbf5d1329f47fb88ba41329b
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-11-23 12:56:59 -0700

    fix: return data when class not prefixed correctly

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,17 @@ function colorizeNode(node: parse5.AST.HtmlParser2.Node, theme: Theme = {}): str
         return (node as parse5.AST.HtmlParser2.TextNode).data
     } else if (node.type === 'tag') {
         const hljsClass = /hljs-(\w+)/.exec((node as parse5.AST.HtmlParser2.Element).attribs.class)
-        const token = hljsClass[1]
-        const nodeData = (node as parse5.AST.HtmlParser2.Element).childNodes
-            .map(node => colorizeNode(node, theme))
-            .join('')
-        return ((theme as any)[token] || (DEFAULT_THEME as any)[token] || plain)(nodeData)
+        if (hljsClass) {
+            const token = hljsClass[1]
+            const nodeData = (node as parse5.AST.HtmlParser2.Element).childNodes
+                .map(node => colorizeNode(node, theme))
+                .join('')
+            return ((theme as any)[token] || (DEFAULT_THEME as any)[token] || plain)(nodeData)
+        }
+
+        // Return the data itself when the class name isn't prefixed with a highlight.js token prefix.
+        // This is common in instances of sublanguages (JSX, Markdown Code Blocks, etc.)
+        return (node as parse5.AST.HtmlParser2.Element).childNodes.map(node => colorizeNode(node, theme)).join('')
     }
 }
 
