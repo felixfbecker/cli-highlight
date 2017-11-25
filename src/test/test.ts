cf538f6a71a2aba78437402386d9ba63d7b738b7
Felix Becker <felix.b@outlook.com>
2017-11-24 17:55:45 -0800

    test: complete coverage for index.ts

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,27 +1,48 @@
 import c from 'chalk'
 import * as fs from 'fs'
-import { highlight } from '../index'
+import { highlight, listLanguages, supportsLanguage } from '../index'
 
-function test(language: string, code: string): void {
-    it(`should color ${language} correctly`, () => {
-        const highlighted = highlight(code)
+describe('highlight()', () => {
+    function test(language: string, code: string): void {
+        it(`should color ${language} correctly`, () => {
+            const highlighted = highlight(code)
 
-        if (process.env.OUTPUT_CODE_SAMPLES) {
-            console.log(language + ':\n\n' + highlighted)
-        }
+            if (process.env.OUTPUT_CODE_SAMPLES) {
+                console.log(language + ':\n\n' + highlighted)
+            }
 
-        expect(highlighted).toMatchSnapshot()
-    })
-}
-
-const fixtures = fs.readdirSync(`${__dirname}/__fixtures__`)
-
-for (const fixture of fixtures) {
-    const fixturePath = `${__dirname}/__fixtures__/${fixture}`
-
-    if (fs.statSync(fixturePath).isFile()) {
-        const [language] = fixture.split('.')
-
-        test(language, fs.readFileSync(fixturePath, 'utf8'))
+            expect(highlighted).toMatchSnapshot()
+        })
     }
-}
+
+    const fixtures = fs.readdirSync(`${__dirname}/__fixtures__`)
+
+    for (const fixture of fixtures) {
+        const fixturePath = `${__dirname}/__fixtures__/${fixture}`
+
+        if (fs.statSync(fixturePath).isFile()) {
+            const [language] = fixture.split('.')
+
+            test(language, fs.readFileSync(fixturePath, 'utf8'))
+        }
+    }
+})
+
+describe('listLanguages()', () => {
+    it('should list the supported languages', () => {
+        const languages = listLanguages()
+        expect(languages).toBeInstanceOf(Array)
+        expect(languages.length).toBeGreaterThan(0)
+    })
+})
+
+describe('supportsLanguage()', () => {
+    it('should return true if the language is supported', () => {
+        const supports = supportsLanguage('json')
+        expect(supports).toBe(true)
+    })
+    it('should return false if the language is not supported', () => {
+        const supports = supportsLanguage('notsupported')
+        expect(supports).toBe(false)
+    })
+})
